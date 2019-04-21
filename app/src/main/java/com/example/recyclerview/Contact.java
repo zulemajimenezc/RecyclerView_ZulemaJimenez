6391457cbcabab823b82228c09d1e665951466ad
Zulema Jimenez <zcjime02@gmail.com>
2019-04-21 11:38:54 -0500

    jaja

---
+++ app/src/main/java/com/example/recyclerview/Contact.java
@@ -0,0 +1,20 @@
+package com.example.recyclerview;
+
+public class Contact {
+    String name;
+    String image;
+    String phone;
+
+    public Contact(){
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getImage(){
+        return image;
+    }
+    public String getPhone(){
+        return phone;
+    }
+}
